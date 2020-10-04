cb380aa3885b936e934ed0ee861c21ed9a06f678
Vitaliy Derda <derdavitaliy@gmail.com>
2020-10-04 12:16:23 +0300

    added project structure

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+//Core
+import * as firebase from 'firebase/app';
+
+export default firebase.initializeApp({
+	apiKey: 'AIzaSyCmcr_gpVvbMv2racBvLJZctZsDWBWkEwQ',
+	authDomain: 'feedback-app-c2e59.firebaseapp.com',
+	databaseURL: 'https://feedback-app-c2e59.firebaseio.com',
+	projectId: 'feedback-app-c2e59',
+	storageBucket: 'feedback-app-c2e59.appspot.com',
+	messagingSenderId: '375579614119',
+	appId: '1:375579614119:web:de183ee0a56bd389823482',
+});
